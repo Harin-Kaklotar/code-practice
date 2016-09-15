f629bff47f410e46a725aa748dc324939ecd993f
Liju John <liju@apple.com>
2016-09-15 09:06:35 -0700

    added completableFuture example

--- java-8/src/main/java/completableFuture/Shop.java
+++ java-8/src/main/java/completableFuture/Shop.java
@@ -6,10 +6,13 @@ import java.util.List;
 import java.util.Random;
 import java.util.concurrent.*;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by liju on 9/6/16.
  */
+
+
 public class Shop {
     private String shopName;
     Random random = new Random();
@@ -27,12 +30,13 @@ public class Shop {
     }
 
     private double calculatePrice(String product) {
+        // simulation using delay for long computation
         delay(1000);
         return random.nextDouble() * product.charAt(0);
 
     }
 
-    public static void delay (long timeInMs){
+    private static void delay (long timeInMs){
         try {
             Thread.sleep(timeInMs);
         } catch (InterruptedException e) {
@@ -82,20 +86,31 @@ class Discount{
         }
     }
 
+    // method to apply discount based on Quote
     public static String applyDiscount(Quote quote){
         return quote.getShopName()+ "price is "+Discount.apply(quote.getPrice(),quote.getDiscountCode());
     }
+
     private static double apply(double price , Discount.Code code){
         delay(1000);
         return (price * (100 - code.discountPercent)/100);
     }
 
-    public static void delay (long timeInMs){
+    private static void delay (long timeInMs){
         try {
             Thread.sleep(timeInMs);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
+}
+
+
+class Exchange{
+    public enum Currency{USD,EUR}
+
+    public static double getRate(Currency usd,Currency eur){
+        return 1.25;
     }
 }
 // Quote
@@ -158,43 +173,51 @@ class Client2 {
     public static void main(String[] args) {
         List<Shop> shops = Arrays.asList(new Shop("iShop"),new Shop("jShop"),new Shop("kShop"),new Shop("lShop"),new Shop("mShop"));
         //find price sequentially
-        long startTime1  = System.currentTimeMillis();
+        long startTime  = System.currentTimeMillis();
         System.out.println(findPriceSequentially(shops));
-        long timeTaken1  = System.currentTimeMillis() - startTime1;
-        System.out.println("Total time taken sequentially is "+timeTaken1);
+        long timeTaken  = System.currentTimeMillis() - startTime;
+        System.out.println("Total time taken sequentially is "+timeTaken);
 
         //find price using parallel streams but not async call
-        long startTime2  = System.currentTimeMillis();
+        startTime  = System.currentTimeMillis();
         System.out.println(findPriceParallelStream(shops));
-        long timeTaken2  = System.currentTimeMillis() - startTime2;
-        System.out.println("Total time taken using parallel stream is "+timeTaken2);
+        timeTaken  = System.currentTimeMillis() - startTime;
+        System.out.println("Total time taken using parallel stream is "+timeTaken);
 
         //find price in Async using completableFuture
-        long startTime3  = System.currentTimeMillis();
+        startTime  = System.currentTimeMillis();
         System.out.println(findPricesAsync(shops));
-        long timeTaken3  = System.currentTimeMillis() - startTime3;
-        System.out.println("Total time taken in async using completableFuture "+timeTaken3);
+        timeTaken  = System.currentTimeMillis() - startTime;
+        System.out.println("Total time taken in async using completableFuture "+timeTaken);
 
         //find price in Async using completableFuture and custom executor
-        long startTime4  = System.currentTimeMillis();
+        startTime  = System.currentTimeMillis();
         System.out.println(findPricesAsyncUsingCustomExecutor(shops));
-        long timeTaken4  = System.currentTimeMillis() - startTime4;
-        System.out.println("Total time taken in async using completableFuture and custom executor "+timeTaken4);
+        timeTaken  = System.currentTimeMillis() - startTime;
+        System.out.println("Total time taken in async using completableFuture and custom executor "+timeTaken);
 
          /* Calling additional service to get discounted price */
 
         //find discounted price sequentially
-        long startTime5  = System.currentTimeMillis();
+        startTime  = System.currentTimeMillis();
         System.out.println(findPricesWithDiscountSequentially(shops));
-        long timeTaken5  = System.currentTimeMillis() - startTime5;
-        System.out.println("Total time taken in ms :: sequentially :: discounted price :: "+timeTaken5);
+        timeTaken  = System.currentTimeMillis() - startTime;
+        System.out.println("Total time taken in ms :: sequentially :: discounted price :: "+timeTaken);
 
         //find discounted price asynchronously
-        long startTime6  = System.currentTimeMillis();
+        startTime  = System.currentTimeMillis();
         System.out.println(findPricesWithDiscountAsync(shops));
-        long timeTaken6  = System.currentTimeMillis() - startTime6;
-        System.out.println("Total time taken in ms :: async :: discounted price :: "+timeTaken6);
+        timeTaken  = System.currentTimeMillis() - startTime;
+        System.out.println("Total time taken in ms :: async :: discounted price :: "+timeTaken);
 
+
+        //find discounted price asynchronously and react to each as and when each is completed
+        long start  = System.currentTimeMillis();
+        final Stream<CompletableFuture<String>> futureStream = findPricesWithDiscountAsyncRefactored(shops);
+        final CompletableFuture[] futuresArray = futureStream.map(f -> f.thenAccept(s -> System.out.println(s + " completed in " + (System.currentTimeMillis() - start) + "ms")))
+            .toArray(size -> new CompletableFuture[size]);
+        CompletableFuture.allOf(futuresArray).join();
+        System.out.println("All shops responded in " + (System.currentTimeMillis() - start) + "ms");
     }
 
     //finding price sequentially
@@ -261,9 +284,9 @@ class Client2 {
         final List<CompletableFuture<String>> futurePrices = shops.stream()
             // asynchronously get the price with discount coupon
             .map(shop -> CompletableFuture.supplyAsync(() -> shop.getPriceWithDiscountCoupon("Product A")))
-            // synchronously get the quote
+                // synchronously get the quote
             .map(future -> future.thenApply(Quote::parse))
-            // asynchronously apply the discount
+                // asynchronously apply the discount
             .map(future -> future.thenCompose(quote -> CompletableFuture.supplyAsync(() -> Discount.applyDiscount(quote), executor)))
             .collect(Collectors.toList());
 
@@ -271,7 +294,74 @@ class Client2 {
         return futurePrices.stream()
             .map(CompletableFuture::join)
             .collect(Collectors.toList());
+    }
 
+    //refactored for reacting to CompletableFuture
+    public static Stream<CompletableFuture<String>> findPricesWithDiscountAsyncRefactored(List<Shop> shops){
+
+        final Executor executor = Executors.newFixedThreadPool(Math.min(shops.size(), 100), runnable -> {
+            Thread thread = new Thread(runnable);
+            thread.setDaemon(true);
+            return thread;
+        });
+
+        //return stream of completable futures
+        return  shops.stream()
+            // asynchronously get the price with discount coupon
+            .map(shop -> CompletableFuture.supplyAsync(() -> shop.getPriceWithDiscountCoupon("Product A")))
+            // synchronously get the quote
+            .map(future -> future.thenApply(Quote::parse))
+            // asynchronously apply the discount
+            .map(future -> future.thenCompose(quote -> CompletableFuture.supplyAsync(() -> Discount.applyDiscount(quote), executor)));
+
+    }
+
+    // combining two independent CompletableFutures
+    public static List<CompletableFuture<Double>> findPriceWithExchangeRate(List<Shop> shops){
+
+        return shops.stream()
+            .map(shop -> CompletableFuture.supplyAsync(() ->shop.getPrice("Product A")))
+            // thenCombine  -> bi-function is executed when this and the other given stage both complete normally,
+            // with the two results as arguments to the supplied bi-function
+            .map(future -> future.thenCombine(CompletableFuture.supplyAsync(() -> Exchange.getRate(Exchange.Currency.USD, Exchange.Currency.EUR)), (price, rate) -> price * rate))
+            .collect(Collectors.toList());
+    }
+}
+
+// shows normal future use using executor service
+class Client3 {
+    public static void main(String[] args) {
+        //create executor service and submit your task to thread pool
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        //Execute a long running operation asynchronously using ExecutorService
+        final Future<Double> future = executorService.submit(new Callable<Double>() {
+            @Override public Double call() throws Exception {
+                return doTimeConsumingTask();
+            }
+
+            public Double doTimeConsumingTask(){
+                // do time consuming calculations
+                return 0.0;
+            }
+
+        });
+
+        // to other stuff while async task is in progress
+        doSomethingElse();
+
+        try {
+            // retrieve the result of the async operation
+            final Double result = future.get(1, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            //Computation exception occurred
+        } catch (ExecutionException e) {
+            //current thread was interrupted while waiting
+        } catch (TimeoutException e) {
+            // timeout occurred before the future completed
+        }
+
+    }
+    public static void doSomethingElse(){
     }
 
 }
