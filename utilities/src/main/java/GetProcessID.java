170e302d4c3fbf8cde5d1592a8d00d124d572fc2
Liju John <liju@apple.com>
2016-11-09 15:37:06 -0800

    added process id util

---
+++ utilities/src/main/java/GetProcessID.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by liju on 11/9/16.
+ */
+public class GetProcessID {
+    public static void main(String[] args) throws IOException {
+        Process process = Runtime.getRuntime().exec(new String[] { "/bin/sh", "-c", "echo $PPID" });
+
+        StringBuilder sb = null;
+        if (process.isAlive()) {
+            InputStream is = null;
+            BufferedReader br = null;
+            try {
+                is = process.getInputStream();
+                br = new BufferedReader(new InputStreamReader(is));
+                sb = new StringBuilder();
+                String line;
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+            } finally {
+                br.close();
+                is.close();
+            }
+        }
+        System.out.println("Process id  = " + sb);
+    }
+}
