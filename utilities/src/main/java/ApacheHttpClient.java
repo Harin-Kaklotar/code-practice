7611ac0d65a3f5c861fd7e998a41d88963ddfa5a
Liju John <liju@apple.com>
2016-11-10 11:10:11 -0800

    Added apachehttpclient example

--- utilities/src/main/java/ApacheHttpClient.java
+++ utilities/src/main/java/ApacheHttpClient.java
@@ -6,13 +6,15 @@ import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicCredentialsProvider;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.util.EntityUtils;
 
 /**
- * HTTP GET and POST using HTTPURLConnection
+ * HTTP GET and POST using ApacheHTTPClient api
  */
 public class ApacheHttpClient {
 
@@ -33,23 +35,41 @@ public class ApacheHttpClient {
     /**
      * HTTP GET
      */
-    public void get(){
+    public void get() {
         CloseableHttpResponse response = null;
-        String responseStr;
         try {
-            String url  = "http://requestb.in/oiimudoi";
+            String url = "http://httpbin.org/get";
             response = closeableHttpClient.execute(new HttpGet(url));
 
-            if (response.getEntity() != null) {
-                if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
-                    responseStr = EntityUtils.toString(response.getEntity());
-
-                    System.out.println(responseStr);
-                } else {
-                    System.out.println("Response not ok");
-                }
+            if (response.getEntity() != null && response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                System.out.println("GET response" + EntityUtils.toString(response.getEntity()));
             }
+            System.out.println("GET response status" + response.getStatusLine());
 
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (response != null)
+                    response.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void post(String jsonString) {
+        CloseableHttpResponse response = null;
+        try {
+            String url = "http://httpbin.org/post";
+            final HttpPost request = new HttpPost(url);
+            request.setHeader("content-type", "application/json");
+            request.setEntity(new StringEntity(jsonString));
+            response = closeableHttpClient.execute(request);
+            if (response.getEntity() != null && response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+                System.out.println("POST response" + EntityUtils.toString(response.getEntity()));
+            }
+            System.out.println("POST response status" + response.getStatusLine());
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -65,5 +85,6 @@ public class ApacheHttpClient {
     public static void main(String[] args) {
         ApacheHttpClient client = new ApacheHttpClient(false);
         client.get();
+        client.post("{\"key\":\"value\"}");
     }
 }
