d90ff78e46597607f7cbefbe34e7ee9cf826dcd9
Liju John <liju@apple.com>
2016-12-05 21:43:58 -0800

    path sum binary tree

---
+++ problem-solving/src/main/java/binaryTree/RootToLeafPaths.java
@@ -0,0 +1,46 @@
+package binaryTree;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Created by liju on 12/5/16.
+ *
+ Given a binary tree, return all root-to-leaf paths.
+
+ For example, given the following binary tree:
+
+    1
+  /   \
+ 2     3
+  \
+  5
+ All root-to-leaf paths are:
+
+ ["1->2->5", "1->3"]
+ */
+public class RootToLeafPaths {
+
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> paths  = new LinkedList<>();
+        return binaryTreePaths(root,paths);
+    }
+
+    public List<String> binaryTreePaths(TreeNode root,List<String> paths){
+
+        //todo
+    }
+
+
+
+    class TreeNode{
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        public TreeNode(int val) {
+            this.val = val;
+        }
+    }
+}
