0c045ca90dc0c219107efa8b439b7fca5cce3916
Liju John <liju@apple.com>
2016-12-05 21:33:16 -0800

    path sum binary tree

--- problem-solving/src/main/java/binaryTree/PathSum.java
+++ problem-solving/src/main/java/binaryTree/PathSum.java
@@ -1,10 +1,10 @@
 package binaryTree;
 
 /**
- * Created by liju on 12/5/16.
- Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+ * Created by liju on 12/5/16. Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that
+ * adding up all the values along the path equals the given sum.
+ *
 
- For example:
  Given the below binary tree and sum = 22,
       5
      / \
@@ -15,22 +15,24 @@ package binaryTree;
  7   2      1
  return true, as there exist a root-to-leaf path 5->4->11->2 which sum is 22.
 
- https://leetcode.com/problems/path-sum/
 
-
+ * 
+ * https://leetcode.com/problems/path-sum/
  */
 public class PathSum {
 
-
     public boolean hasPathSum(TreeNode root, int sum) {
+        if (root==null) return false;
+        if (root.val-sum == 0 && root.left==null && root.right==null) return true;
 
-        return true;
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
     }
 
-    class TreeNode{
+    class TreeNode {
         int val;
-        SameTree.TreeNode left;
-        SameTree.TreeNode right;
+        TreeNode left;
+        TreeNode right;
+
         public TreeNode(int val) {
             this.val = val;
         }
