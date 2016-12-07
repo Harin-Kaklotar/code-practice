8dc29fa6e1af4e32491405d5992be433283fb15d
Liju John <liju@apple.com>
2016-12-07 10:56:47 -0800

    IsValidBST

--- problem-solving/src/main/java/binaryTree/ValidBST.java
+++ problem-solving/src/main/java/binaryTree/ValidBST.java
@@ -7,9 +7,9 @@ package binaryTree;
  * 
  * Assume a BST is defined as follows:
  * 
- * - The left subtree of a node contains only nodes with keys less than the node's key. - The right subtree of a node
- * contains only nodes with keys greater than the node's key. - Both the left and right subtrees must also be binary
- * search trees.
+ - The left subtree of a node contains only nodes with keys less than the node's key.
+ - The right subtree of a node contains only nodes with keys greater than the node's key.
+ - Both the left and right subtrees must also be binary search trees.
  * 
  * https://leetcode.com/problems/validate-binary-search-tree/
  */
