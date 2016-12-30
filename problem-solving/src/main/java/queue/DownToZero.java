dd76f9d12185c12384910899d885bc9c4bdfc7c7
liju <lijubjohn@gmail.com>
2016-12-29 23:17:05 -0800

    version 1 - need performance improvement

--- problem-solving/src/main/java/queue/DownToZero.java
+++ problem-solving/src/main/java/queue/DownToZero.java
@@ -15,47 +15,63 @@ public class DownToZero {
             numbers[i] = sc.nextInt();
         }
 
-        Map<Integer, Integer> computedNumbers = new HashMap<>();
+        Map<Integer, Integer> memoize = new HashMap<>();
         Queue<Pair> queue = new LinkedList<>();
 
         for (int i = 0; i < numbers.length; i++) {
-            queue.clear();
-            queue.offer(new Pair(numbers[i], 1, 0));
-            int minStep = reduceN(queue);
+            queue.offer(new Pair(numbers[i], 0));
+            int minStep = processNumber(queue, memoize);
             System.out.println(minStep);
+            memoize.clear();
+            queue.clear();
         }
 
     }
 
-    private static int reduceN(Queue<Pair> queue) {
+    private static int processNumber(Queue<Pair> queue, Map<Integer, Integer> memoize) {
         while (!queue.isEmpty()) {
             Pair pair = queue.poll();
-            int max = Math.max(pair.x, pair.y);
-            if (max == 1) {
+
+            if (memoize.containsKey(pair.num) && memoize.get(pair.num) < pair.step)
+                continue;
+
+            if (pair.num == 1) {
                 return (pair.step + 1);
             }
-            pushFactorsOfNum(pair, queue);
+            reduceNumber(pair, queue, memoize);
         }
         return -1;
     }
 
-    private static void pushFactorsOfNum(Pair pair, Queue<Pair> queue) {
-        int max = Math.max(pair.x, pair.y);
-        for (int i = 2; i <= Math.sqrt(max); i++) {
-            int rem = max % i;
+    private static void reduceNumber(Pair pair, Queue<Pair> queue, Map<Integer, Integer> memoize) {
+
+        for (int i = 2; i <= Math.sqrt(pair.num); i++) {
+            int rem = pair.num % i;
             if (rem == 0) {
-                queue.offer(new Pair(max / i, i, pair.step + 1));
+                //int max = Math.max(i, pair.num / i); // no need to compare ,later is always the max
+                int max = pair.num / i;
+                int step = pair.step + 1;
+                if (memoize.containsKey(max) && memoize.get(max) < step)
+                    continue;
+
+                memoize.put(max, step);
+                queue.offer(new Pair(max, step));
             }
         }
-        queue.offer(new Pair(max - 1, 1, pair.step + 1));
+
+        int num = pair.num - 1;
+        int step = pair.step + 1;
+        if (memoize.containsKey(num) && memoize.get(num) < step) return;
+
+        memoize.put(num, step);
+        queue.offer(new Pair(num, step));
     }
 
     static class Pair {
-        int x, y, step;
+        int num, step;
 
-        public Pair(int x, int y, int step) {
-            this.x = x;
-            this.y = y;
+        public Pair(int num, int step) {
+            this.num = num;
             this.step = step;
         }
     }
