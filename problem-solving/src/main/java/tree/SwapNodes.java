f14b8e0004cce7bc69c948f6f7f54af1ecefc10d
liju <lijubjohn@gmail.com>
2017-01-06 20:36:07 -0800

    solution to swap nodes - trees

--- problem-solving/src/main/java/tree/SwapNodes.java
+++ problem-solving/src/main/java/tree/SwapNodes.java
@@ -13,7 +13,7 @@ public class SwapNodes {
         Tree tree = new Tree();
 
         for (int i = 1; i <= n; i++) {
-            tree.add(sc.nextInt(),sc.nextInt());
+            tree.add(sc.nextInt(), sc.nextInt());
         }
 
         final int T = sc.nextInt();
@@ -25,10 +25,11 @@ public class SwapNodes {
     }
 
 
-    private static class Tree{
+    private static class Tree {
 
         private static Queue<Node> queue = new LinkedList<>();
-        private static class Node{
+
+        private static class Node {
             int index;
             Node left;
             Node right;
@@ -38,17 +39,20 @@ public class SwapNodes {
             }
         }
 
-        static private Node root=new Node(1);
+        static private Node root = new Node(1);
+
         static {
             queue.add(root);
         }
 
-        public void add(int left ,int right){
+        public void add(int left, int right) {
             final Node node = queue.poll();
             node.left = new Node(left);
             node.right = new Node(right);
-            queue.offer(node.left);
-            queue.offer(node.right);
+            if (node.left.index != -1)
+                queue.offer(node.left);
+            if (node.right.index != -1)
+                queue.offer(node.right);
         }
 
         public void inOrderTraversal() {
@@ -57,22 +61,22 @@ public class SwapNodes {
         }
 
         private void inOrderTraversalRecur(Node root) {
-            if (root.index==-1) return;
+            if (root.index == -1) return;
             inOrderTraversalRecur(root.left);
-            System.out.print(root.index+" ");
+            System.out.print(root.index + " ");
             inOrderTraversalRecur(root.right);
         }
 
-        public void swap(int k){
-            int maxDepth  = maxDepthOfTree(root);
+        public void swap(int k) {
+            int maxDepth = maxDepthOfTree(root);
             for (int i = 1; ; i++) {
                 final int t = i * k;
-                if (t<=maxDepth) {
-                    List<Node> nodes = nodesAtdepth(root,i * k);
+                if (t <= maxDepth) {
+                    List<Node> nodes = nodesAtdepth(root, i * k);
                     for (Node node : nodes) {
                         swapChild(node);
                     }
-                }else {
+                } else {
                     break;
                 }
             }
@@ -85,29 +89,24 @@ public class SwapNodes {
             return list;
         }
 
-        private void nodesAtDepthRec(Node root, int depth,List<Node> list) {
-            if (root==null) return;
-            if (depth==1){
+        private void nodesAtDepthRec(Node root, int depth, List<Node> list) {
+            if (root == null) return;
+            if (depth == 1) {
                 list.add(root);
-            }else {
+            } else {
                 nodesAtDepthRec(root.left, depth - 1, list);
                 nodesAtDepthRec(root.right, depth - 1, list);
             }
         }
 
         private int maxDepthOfTree(Node root) {
-           return maxDepthRec(root, 1);
-        }
-
-        private int maxDepthRec(Node root, int i) {
-            if (root.left==null) return i;
-            //since it is a complete binary tree,traverse any branch
-            return maxDepthRec(root.left, i + 1);
+            if (root.index == -1) return 0;
+            return Math.max(maxDepthOfTree(root.left), maxDepthOfTree(root.right)) + 1;
         }
 
         private void swapChild(Node node) {
-            Node tmp =node.left;
-            node.left=node.right;
+            Node tmp = node.left;
+            node.left = node.right;
             node.right = tmp;
         }
     }
