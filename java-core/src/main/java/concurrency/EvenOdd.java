b939507431b0a4ee10a7e20f15e479cad1cfad27
liju <lijubjohn@gmail.com>
2020-08-12 19:39:47 +0530

    added even odd thread program

---
+++ java-core/src/main/java/concurrency/EvenOdd.java
@@ -0,0 +1,61 @@
+package concurrency;
+
+/**
+ * Created by liju on 8/12/20.
+ */
+public class EvenOdd {
+    private boolean printEven = true;
+    private int max = 20;
+    private int i = 0;
+
+    public static void main(String[] args) {
+
+        EvenOdd evenOdd = new EvenOdd();
+
+        new Thread(() -> evenOdd.printEven()).start();
+        new Thread(() -> evenOdd.printOdd()).start();
+
+    }
+
+    public void printEven() {
+        while (i < max) {
+            synchronized (this) {
+                while (!printEven) {
+                    try {
+                        this.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("even : num=" + i);
+                printEven = false;
+                i++;
+                this.notifyAll();
+            }
+        }
+
+    }
+
+    public void printOdd() {
+        while (i < max) {
+            synchronized (this) {
+                while (printEven) {
+                    try {
+                        this.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                System.out.println("odd : num=" + i);
+                printEven = true;
+                i++;
+                this.notifyAll();
+            }
+        }
+
+    }
+
+
+}
+
+
